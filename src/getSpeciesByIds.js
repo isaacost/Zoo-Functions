a2ddd99f642f36e63de41e9955fe75074882d352
Isa Costa <isa-hcosta@hotmail.com>
2022-07-28 15:48:08 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+
+function getSpeciesByIds(...ids) {
+  const resultado = [];
+
+  ids.forEach((id, index) => {
+    if (species.some((element) => element.id === id)) {
+      resultado[index] = species.find((element) => element.id === id);
+    }
+  })
+  return resultado;
 }
+
 
 module.exports = getSpeciesByIds;
