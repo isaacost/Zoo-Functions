6e5db40afc6544b0ebbde51ee8ac60af1bec5f51
Isa Costa <isa-hcosta@hotmail.com>
2022-07-28 18:34:46 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  } return data.employees.find((funcionario) =>
+    funcionario.firstName === employeeName || funcionario.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
