9dc9a60e6ff21f1be2ebf984bb9fe9725a5ffb78
Isa Costa <isa-hcosta@hotmail.com>
2022-07-28 20:33:18 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const quantidade = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      quantidade.child += 1;
+    } if (element.age >= 18 && element.age < 50) {
+      quantidade.adult += 1;
+    } if (element.age >= 50) {
+      quantidade.senior += 1;
+    }
+  });
+  return quantidade;
 }
+const precos = data.prices;
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) return 0;
+  if (entrants.length === undefined) return 0;
+  const pessoas = countEntrants(entrants);
+  let valor = 0;
+  const { child, adult, senior } = pessoas;
+  const { adult: precoAdult, senior: precoSenior, child: precoChild } = precos;
+  valor = (child * precoChild) + (adult * precoAdult) + (senior * precoSenior);
+  return valor;
 }
 
 module.exports = { calculateEntry, countEntrants };
