39f076fa75ea1f166376e75d1040aa77c92be527
Isa Costa <isa-hcosta@hotmail.com>
2022-07-29 15:27:33 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+  const animalID = data.employees.find((employee) => employee.id === id).responsibleFor[0];
 
+  const resultado = data.species.find((especie) => especie.id === animalID).residents
+    .reduce((maisVelho, atual) => {
+      if (atual.age > maisVelho.age) {
+        return atual;
+      }
+      return maisVelho;
+    });
+  return [resultado.name, resultado.sex, resultado.age];
+}
 module.exports = getOldestFromFirstSpecies;
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
