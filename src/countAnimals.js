720872aed3def8e5b719a5c8f52f96061a2f567c
Isa Costa <isa-hcosta@hotmail.com>
2022-07-29 14:56:15 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, especie) => {
+      acc[especie.name] = especie.residents.length;
+      return acc;
+    }, {});
+  }
+  if (animal.specie && animal.sex) {
+    return data.species.find((especie) => especie.name === animal.specie).residents
+      .filter((residente) => residente.sex === animal.sex).length;
+  }
+  if (animal.specie) {
+    return data.species.find((especie) => especie.name === animal.specie).residents.length;
+  }
 }
 
 module.exports = countAnimals;
