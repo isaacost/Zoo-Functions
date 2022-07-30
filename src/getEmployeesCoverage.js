02157268f8871b40e4080e448f1db79439662f58
Isa Costa <isa-hcosta@hotmail.com>
2022-07-29 22:00:46 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+function infoEmpregados(employee) {
+  const employeeID = employee.id;
+  const nomeCompleto = `${employee.firstName} ${employee.lastName}`;
+  const nomeAnimal = employee.responsibleFor.map((animalID) => species
+    .find((especie) => especie.id === animalID).name);
+  const localizacaoAnimal = nomeAnimal.map((nome) => species
+    .find((especie) => especie.name === nome).location);
+
+  return {
+    id: employeeID,
+    fullName: nomeCompleto,
+    species: nomeAnimal,
+    locations: localizacaoAnimal,
+  };
+}
+
+function getEmployeesCoverage(parametro) {
+  if (!parametro) {
+    return employees.map((employee) => infoEmpregados(employee));
+  }
+  const { name, id } = parametro;
+  let resultado;
+  if (name) {
+    resultado = (element) => element.firstName === name || element.lastName === name;
+  } if (id) {
+    resultado = (element) => element.id === id;
+  }
+  const employee = employees.find(resultado);
+  if (!employee) {
+    throw new Error('Informações inválidas');
+  }
+  return infoEmpregados(employee);
 }
 
 module.exports = getEmployeesCoverage;
