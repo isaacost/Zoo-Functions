2ee512202492bdf83a55c63ff6e5377ef7e7ad80
Isa Costa <isa-hcosta@hotmail.com>
2022-08-02 13:24:12 -0300

    requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,49 @@
 const data = require('../data/zoo_data');
 
+function mapa() {
+  const local = { NE: [], NW: [], SE: [], SW: [] };
+  return data.species.reduce((acc, especie) => {
+    acc[especie.location].push(especie.name);
+    return acc;
+  }, local);
+}
+
+function residentes(animal, sorted, sex) {
+  const { residents } = data.species.find((especie) => especie.name === animal);
+
+  if (sorted && sex) {
+    return residents.filter((residente) => residente.sex === sex)
+      .map((residente) => residente.name).sort();
+  }
+  if (sex) {
+    return residents.filter((residente) => residente.sex === sex)
+      .map((residente) => residente.name);
+  }
+  if (sorted) {
+    return residents.map((residente) => residente.name).sort();
+  }
+  return residents.map((residente) => residente.name);
+}
+
 function getAnimalMap(options) {
-  // seu código aqui
+  const funcMapa = mapa();
+
+  if (!options) return funcMapa;
+
+  const { includeNames, sorted, sex } = options;
+
+  if (includeNames) {
+    Object.keys(funcMapa).forEach((loc) => {
+      funcMapa[loc] = funcMapa[loc].map((animal) => {
+        const animalNovo = {};
+        animalNovo[animal] = residentes(animal, sorted, sex);
+        return animalNovo;
+      });
+    });
+    return funcMapa;
+  }
+  if (sex) return funcMapa;
+  if (sorted) return funcMapa;
 }
 
 module.exports = getAnimalMap;
